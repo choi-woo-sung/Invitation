64b53c7afa78f35bd07bf726b1d8a7eaf3e635af
Kong <63629853+choi-woo-sung@users.noreply.github.com>
2024-05-14 01:40:35 +0900

    111

--- app/src/main/java/com/woosung/invitation/CardDeckActivity.kt
+++ app/src/main/java/com/woosung/invitation/CardDeckActivity.kt
@@ -1,37 +1,122 @@
 package com.woosung.invitation
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun CardDeckActivity() {
-    LazyVerticalGrid(columns = GridCells.Fixed(5)) {
-        items(items = SampleCardDeck.testCard) {
-            when (it) {
-                is InvitationCard.NormalCard -> NormalCard(isLocked = it.isLocked, frontImage = it.image)
+    var showDetails by remember { mutableStateOf(true) }
+    var selectedCard by remember { mutableStateOf("") }
+
+    SharedTransitionLayout {
+        AnimatedContent(
+            targetState = showDetails,
+        ) { it ->
+            if (it) {
+                LazyVerticalGrid(columns = GridCells.Fixed(5)) {
+                    itemsIndexed(items = SampleCardDeck.testCard) { idx, it ->
+                        when (it) {
+                            is InvitationCard.NormalCard -> NormalCard(
+                                isLocked = it.isLocked,
+                                frontImage = it.image,
+                                sharedTransitionScope = this@SharedTransitionLayout,
+                                animatedVisibilityScope = this@AnimatedContent,
+                                onClick = { key ->
+                                    showDetails = false
+                                    selectedCard = key
+                                },
+                                key = idx.toString()
+                            )
+                        }
+                    }
+                }
+            } else {
+                NormalCard(
+                    modifier = Modifier,
+                    isLocked = false,
+                    frontImage = R.drawable.img_card1_front,
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    animatedVisibilityScope = this@AnimatedContent,
+                    key = selectedCard,
+                    onClick = {
+                        showDetails = true
+                    }
+                )
             }
         }
     }
 }
 
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun NormalCard(modifier: Modifier = Modifier, isLocked: Boolean, @DrawableRes frontImage: Int) {
-    Box() {
-        if (isLocked) {
-            Image(painter = painterResource(id = R.drawable.img_back), contentDescription = "카드 뒷면")
-        } else {
-            Image(painter = painterResource(id = frontImage), contentDescription = "카드 앞면")
+fun NormalCard(
+    modifier: Modifier = Modifier,
+    isLocked: Boolean,
+    @DrawableRes frontImage: Int,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    key: String,
+    onClick: (key: String) -> Unit = {}
+) {
+    with(sharedTransitionScope) {
+        Box(
+            modifier = modifier
+                .sharedElement(
+                    rememberSharedContentState(key = key),
+                    animatedVisibilityScope = animatedVisibilityScope
+                )
+        ) {
+            if (isLocked) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(id = R.drawable.img_back),
+                    contentDescription = "카드 뒷면",
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = frontImage),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            onClick(key)
+                        },
+                    contentDescription = "카드 앞면"
+                )
+            }
+        }
+    }
+    if (isLocked) {
+        TextButton(onClick = { /*TODO*/ }) {
+
         }
     }
 }
@@ -45,10 +130,58 @@ private fun CardDeckActivityPreview() {
 
 
 sealed class InvitationCard(open val isLocked: Boolean = false, open val image: Int) {
-    data class NormalCard(override val isLocked: Boolean = false, @DrawableRes override val image: Int) : InvitationCard(isLocked, image)
+    data class NormalCard(
+        override val isLocked: Boolean = false,
+        @DrawableRes override val image: Int
+    ) : InvitationCard(isLocked, image)
 }
 
 
 object SampleCardDeck {
-    val testCard = listOf<InvitationCard>(InvitationCard.NormalCard(isLocked = false, image = R.drawable.img_card1_front))
+    val testCard = listOf<InvitationCard>(
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front,
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+        InvitationCard.NormalCard(
+            isLocked = false,
+            image = R.drawable.img_card1_front
+        ),
+    )
 }
--- app/src/main/java/com/woosung/invitation/animateBounds.kt
+++ app/src/main/java/com/woosung/invitation/animateBounds.kt
@@ -1,53 +1,53 @@
-package com.woosung.invitation
-
-import androidx.compose.animation.core.DeferredTargetAnimation
-import androidx.compose.animation.core.ExperimentalAnimatableApi
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.tween
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.layout.LookaheadScope
-import androidx.compose.ui.layout.approachLayout
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.round
-
-
-//context reciver는 무엇일까?
-//아 확장함수 할때 장점이 있긴 하것네.. 둘다 확장이 필요한데 확장함수는 하나만 만들수 있으니까
-context(LookaheadScope)
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimatableApi::class)
-fun Modifier.animateBounds(): Modifier = composed {
-    val offsetAnim = remember { DeferredTargetAnimation(IntOffset.VectorConverter) }
-    val sizeAnim = remember { DeferredTargetAnimation(IntSize.VectorConverter) }
-    val scope = rememberCoroutineScope()
-    this.approachLayout(
-        isMeasurementApproachComplete = {
-            sizeAnim.updateTarget(it, scope, tween(2000))
-            sizeAnim.isIdle
-        },
-        isPlacementApproachComplete = {
-            val target = lookaheadScopeCoordinates.localLookaheadPositionOf(it)
-            offsetAnim.updateTarget(target.round(), scope , tween(2000))
-            offsetAnim.isIdle
-        },
-    ) { measurable, constraints ->
-        val (animWidth, animHeight) = sizeAnim.updateTarget(lookaheadSize, scope)
-        measurable.measure(Constraints.fixed(animWidth, animHeight)).run {
-            layout(width, height) {
-                coordinates?.let {
-                    val target = lookaheadScopeCoordinates.localLookaheadPositionOf(it).round()
-                    val animOffset = offsetAnim.updateTarget(target, scope)
-                    val current = lookaheadScopeCoordinates.localPositionOf(it, Offset.Zero).round()
-                    val (x, y) = animOffset - current
-                    place(x, y)
-                } ?: place(0, 0)
-            }
-        }
-    }
-}
+//package com.woosung.invitation
+//
+//import androidx.compose.animation.core.DeferredTargetAnimation
+//import androidx.compose.animation.core.ExperimentalAnimatableApi
+//import androidx.compose.animation.core.VectorConverter
+//import androidx.compose.animation.core.tween
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.rememberCoroutineScope
+//import androidx.compose.ui.ExperimentalComposeUiApi
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.composed
+//import androidx.compose.ui.geometry.Offset
+//import androidx.compose.ui.layout.LookaheadScope
+//import androidx.compose.ui.layout.approachLayout
+//import androidx.compose.ui.unit.Constraints
+//import androidx.compose.ui.unit.IntOffset
+//import androidx.compose.ui.unit.IntSize
+//import androidx.compose.ui.unit.round
+//
+//
+////context reciver는 무엇일까?
+////아 확장함수 할때 장점이 있긴 하것네.. 둘다 확장이 필요한데 확장함수는 하나만 만들수 있으니까
+//context(LookaheadScope)
+//@OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimatableApi::class)
+//fun Modifier.animateBounds(): Modifier = composed {
+//    val offsetAnim = remember { DeferredTargetAnimation(IntOffset.VectorConverter) }
+//    val sizeAnim = remember { DeferredTargetAnimation(IntSize.VectorConverter) }
+//    val scope = rememberCoroutineScope()
+//    this.approachLayout(
+//        isMeasurementApproachComplete = {
+//            sizeAnim.updateTarget(it, scope, tween(2000))
+//            sizeAnim.isIdle
+//        },
+//        isPlacementApproachComplete = {
+//            val target = lookaheadScopeCoordinates.localLookaheadPositionOf(it)
+//            offsetAnim.updateTarget(target.round(), scope , tween(2000))
+//            offsetAnim.isIdle
+//        },
+//    ) { measurable, constraints ->
+//        val (animWidth, animHeight) = sizeAnim.updateTarget(lookaheadSize, scope)
+//        measurable.measure(Constraints.fixed(animWidth, animHeight)).run {
+//            layout(width, height) {
+//                coordinates?.let {
+//                    val target = lookaheadScopeCoordinates.localLookaheadPositionOf(it).round()
+//                    val animOffset = offsetAnim.updateTarget(target, scope)
+//                    val current = lookaheadScopeCoordinates.localPositionOf(it, Offset.Zero).round()
+//                    val (x, y) = animOffset - current
+//                    place(x, y)
+//                } ?: place(0, 0)
+//            }
+//        }
+//    }
+//}
